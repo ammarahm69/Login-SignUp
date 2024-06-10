79ccbc327e256957b91c41bbc738f54691a70723
Ammar Ahmed <ammar69aa59@gmail.com>
2024-06-10 11:08:36 -0700

    qwe

--- app.js
+++ app.js
@@ -6,46 +6,52 @@ document.addEventListener('DOMContentLoaded', () => {
     const eye = document.getElementById('eye');
     const createAccountBtn = document.getElementById('create-Account');
     const signBtn = document.getElementById('submit');
-  
+    
+    
     createAccountBtn?.addEventListener('click', () => {
-      const nameValue = username.value;
-      const phoneValue = phone.value;
-      const emailValue = email.value;
-      const passwordValue = password.value;
-      if (nameValue === '' || passwordValue === '') {
-        alert('Please fill all fields correctly to sign up');
-      } else {
-        localStorage.setItem('name', nameValue);
-        localStorage.setItem('phone', phoneValue);
-        localStorage.setItem('email', emailValue);
-        localStorage.setItem('password', passwordValue);
-        alert('Your account has been created');
-        window.location.href = 'index.html';
-      }
+        const nameValue = username.value;
+        const phoneValue = phone.value;
+        const emailValue = email.value;
+        const passwordValue = password.value;
+        if (nameValue === '' || passwordValue === '') {
+            alert('Please fill all fields correctly to sign up');
+            } else {
+            localStorage.setItem('name', nameValue);
+            localStorage.setItem('phone', phoneValue);
+            localStorage.setItem('email', emailValue);
+            localStorage.setItem('password', passwordValue);
+            alert('Your account has been created');
+            window.location.href = 'index.html';
+        }
     });
-  
+    
     eye?.addEventListener('click', () => {
-      if (password.type === 'password') {
-        password.type = 'text';
-      } else {
-        password.type = 'password';
-      }
+        if (password.type === 'password') {
+            password.type = 'text';
+            } else {
+            password.type = 'password';
+            }
     });
-  
+
     signBtn?.addEventListener('click', signUp);
-  
+
     function signUp() {
-      const storedEmail = localStorage.getItem('email');
-      const storedPassword = localStorage.getItem('password');
-      const storedName = localStorage.getItem('name');
-      const emailValue = email.value;
-      const passwordValue = password.value;
-  
-      if (storedEmail === emailValue && storedPassword === passwordValue) {
-        alert(`Thanks for logging in ${storedName}`);
-      } else {
-        alert('Invalid Email or Password');
-      }
+        const storedEmail = localStorage.getItem('email');
+        const storedPassword = localStorage.getItem('password');
+        const storedName = localStorage.getItem('name');
+        const emailValue = email.value;
+        const passwordValue = password.value;
+
+        if (storedEmail === emailValue && storedPassword === passwordValue) {
+            alert(`Thanks for logging in ${storedName}`);
+            window.open('dashboard.html')
+        } else {
+            alert('Invalid Email or Password');
+            }
     }
-  });
-  
+});
+
+const pageChange = document.getElementById('create-page')
+pageChange.addEventListener('click', () => {
+    window.open('create.html')
+})
